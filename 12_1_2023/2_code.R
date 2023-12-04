1ce1b719dc55d711637783c8d531166d4f4e04c7
joneskyle0 <joneskyle0@gmail.com>
2023-12-04 13:55:58 -0500

    solution to 1_code.R

---
+++ 12_1_2023/2_code.R
@@ -0,0 +1,27 @@
+calibration_document <- read.csv('1_input.txt', header = F, stringsAsFactors = F, col.names = 'hidden_cal_val')
+
+strReverse <- function(x) sapply(lapply(strsplit(x, NULL), rev), paste, collapse = "")
+
+nums <- c('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+
+get_calibration_values <- function(x) {
+  output_1 <- unlist(strsplit(x, '[[:alpha:]]'))
+  output_1 <- noquote(output_1[output_1!=""])[1]
+  output_1 <- if(nchar(output_1) > 1) {substring(output_1, 1, 1)} else {output_1}
+  
+  x <- strReverse(x)
+  output_2 <- unlist(strsplit(x, '[[:alpha:]]'))
+  output_2 <- noquote(output_2[output_2!=""])[1]
+  output_2 <- if(nchar(output_2) > 1) {substring(output_2, 1, 1)} else {output_2}
+ 
+  output <- as.numeric(noquote(paste(output_1, output_2, sep = '')))
+  return(output)
+}
+
+calibration_document$cal_val <- NA
+
+for(i in 1:nrow(calibration_document)) {
+  calibration_document[i,2] <- get_calibration_values(calibration_document[i,1])
+}
+
+sum(calibration_document$cal_val)
